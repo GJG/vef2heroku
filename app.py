739ea3de3791241f11705e339308e0cb1ecbf75a
gjg <gjg@tskoli.is>
2018-09-02 15:03:15 +0000

    run 2

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def index():
 
 
 if os.environ.get('APP_LOCATION') == 'heroku':
-	bottle.run(host='0.0.0.0', port=argv[1])
+	run(host="0.0.0.0", port=int(os.environ.get(argv[1])))
+#	 bottle.run(host='0.0.0.0', port=argv[1])
 #    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 else:
     run(host='localhost', port=8080, debug=True)
