eaaf6382d465b3abf8a73aa02b25222fb28f198c
gjg <gjg@tskoli.is>
2018-09-02 15:17:48 +0000

    run 5

--- app.py
+++ app.py
@@ -10,9 +10,7 @@ bottle.debug(True)
 def index():
     return "<h1>Halló heimur á Heroku hýsingu og Github geymslu</h1>"
 
-bottle.run(host='0.0.0.0', port=argv[1])
-
-# if os.environ.get('APP_LOCATION') == 'heroku':	 
-#    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-# else:
-#    run(host='localhost', port=8080, debug=True)
+if os.environ.get('APP_LOCATION') == 'heroku':	 
+	bottle.run(host='0.0.0.0', port=argv[1])
+else:
+	run(host='localhost', port=8080, debug=True)
