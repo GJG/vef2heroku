995ec6217f48b44415d25a35778651c8e7f97543
Guðmundur Jón Guðjónsson <gjg@tskoli.is>
2018-09-10 15:07:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,40 @@ from sys import argv
 
 import bottle
 from bottle import *
+bottle.debug(True)
+
+ 
+@route("/")
+def index():
+    return """
+    <h2>Verkefnki 1</h1>
+    <a href="/about">About</a>
+    <a href="/bio">Bio</a>
+    <a href="/pic">Pictures</a>
+"""
+
+@route("/about")
+def elvar():
+    return "Hér er sögn um mig"
+
+@route("/bio")
+def elvar():
+    return"Hér er Biograph"
+
+@route("/pic")
+def elvar():
+    return "Hér er ljósmynd"
+
+
+bottle.run(host='0.0.0.0', port=argv[1])
+
+
+
+"""
+from sys import argv
+
+import bottle
+from bottle import *
 
 bottle.debug(True)
 
@@ -10,5 +44,5 @@ def index():
     return "<h1>Halló heimur á Heroku hýsingu og Github geymslu</h1>"
 
 bottle.run(host='0.0.0.0', port=argv[1])
-
+"""
 # run(host='localhost', port=8080, debug=True)
